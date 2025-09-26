13eaf23059f5ab7404fb440df66fcf7d2529853b
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-09-26 13:14:14 +0300

    Initial Commit

---
+++ compose-table/build.gradle.kts
@@ -0,0 +1,88 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `maven-publish`
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.compose)
+}
+
+group = "net.lsafer.compose-table"
+version = "local_snapshot"
+
+kotlin {
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    jvmToolchain(22)
+    jvm("desktop")
+    js { browser() }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs { browser() }
+    sourceSets {
+        val commonMain by getting
+        val jsMain by getting
+        val wasmJsMain by getting
+        val desktopMain by getting
+
+        val skikoCommon by creating
+        skikoCommon.dependsOn(commonMain)
+        jsMain.dependsOn(skikoCommon)
+        wasmJsMain.dependsOn(skikoCommon)
+        desktopMain.dependsOn(skikoCommon)
+    }
+    sourceSets.commonMain.dependencies {
+        implementation(compose.runtime)
+        implementation(compose.foundation)
+        implementation(compose.ui)
+        implementation(compose.material3)
+        implementation(compose.materialIconsExtended)
+    }
+    sourceSets.androidMain.dependencies {
+        implementation(compose.preview)
+    }
+    sourceSets.named("desktopMain").dependencies {
+        implementation(compose.desktop.currentOs)
+    }
+}
+
+android {
+    namespace = "net.lsafer.compose.table"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+    dependencies {
+        debugImplementation(compose.uiTooling)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+rootProject.name = "lsafer-compose-table"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        maven("https://jitpack.io")
+        mavenCentral()
+    }
+}
+
+include(":compose-table")
